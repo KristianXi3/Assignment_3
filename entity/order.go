4676b3fb94746546b4a7c29d55d8cfba49140e6e
KristianXi3 <kristian.xie@gmail.com>
2022-09-07 22:34:14 +0700

    order comit

---
+++ entity/order.go
@@ -0,0 +1,10 @@
+package entity
+
+import "time"
+
+type Order struct {
+	OrderId      int       `json:"orderId"`
+	CustomerName string    `json:"customerName"`
+	OrderedAt    time.Time `json:"orderedAt"`
+	Items        []Item    `json:"items"`
+}
